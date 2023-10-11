4a64d90c2118f04006793bd2d308c535a7d92723
FuexFollets <fuex9follets@gmail.com>
2023-10-11 17:21:59 -0400

    Added constants for formats

---
+++ src/speedograph/constants.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+
+CACHE_DATA_PATH = "data/cache"
+FILENAME_FORMAT = "{name}_{start_epoch}_{end_epoch}.json"
+API_URL = "https://data.typeracer.com/games?playerId=tr:{username}&universe=play&startDate={startEpoch}&endDate={endEpoch}"
